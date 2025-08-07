d1b57e84198199ffcc4df790f32ff07aed22d43d
Starry0114 <128798573+Starry0114@users.noreply.github.com>
2025-08-07 16:13:15 +0800

    Add files via upload

--- db.php
+++ db.php
@@ -1,19 +1,23 @@
 <?php
-// db.php
 class Database {
-    private $host = "localhost";
-    private $db_name = "user_auth";
-    private $username = "root";
-    private $password = "";
+    // Azure Database for MySQL 连接配置
+    private $host = "starry.database.windows.net"; // Azure 服务器地址
+    private $db_name = "website"; // 数据库名称
+    private $username = "Starry0114"; // Azure 管理员用户名
+    private $password = "Dark0689@@"; // 管理员密码
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
         try {
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
-                $this->username,
-                $this->password
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
+                $this->username, 
+                $this->password,
+                [
+                    PDO::MYSQL_ATTR_SSL_CA => '/path/to/BaltimoreCyberTrustRoot.crt.pem',
+                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true
+                ]
             );
             $this->conn->exec("set names utf8");
         } catch(PDOException $e) {
