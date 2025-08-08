29024b6b9e57f25f3e3347096629ab31cc0071e6
Starry0114 <128798573+Starry0114@users.noreply.github.com>
2025-08-08 09:36:25 +0800

    Add files via upload

--- db.php
+++ db.php
@@ -1,10 +1,9 @@
 <?php
 class Database {
-    // Azure Database for MySQL 连接配置
-    private $host = "starry.database.windows.net"; // Azure 服务器地址
-    private $db_name = "website"; // 数据库名称
-    private $username = "Starry0114"; // Azure 管理员用户名
-    private $password = "Dark0689@@"; // 管理员密码
+    private $host = "starry00.mysql.database.azure.com";
+    private $db_name = "starry00";
+    private $username = "starry";
+    private $password = "Dark0689@@";
     public $conn;
 
     public function getConnection() {
